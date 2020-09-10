5d282829f952f83cbf69505709e66071075bde03
HanKienjd <kienp544@gmail.com>
2020-09-10 20:21:45 +0700

    extra code MultipleChoiceResult

--- src/components/body/detail-exam/MultipleChoiceExam.js
+++ src/components/body/detail-exam/MultipleChoiceExam.js
@@ -17,13 +17,11 @@ class MultipleChoiceExam extends React.Component {
   render() {
     return (
       <MainContent>
-        <div className='MultipleChoiceExam'>
-        <div className="exam-infor-panel">
-
-        <div className="Mul-title">Đề luyện thi THPT Quốc gia 2020 môn Lý - Đề số 1</div>
-        <div className="description">
-          Gửi đến các bạn học sinh lớp 12 đề luyện thi THPT Quốc gia 2020 môn Lý - Đề số 1 có đáp án do Học Tốt tổng hợp nhằm giúp các em có thêm tư liệu để tham khảo củng cố kiến thức trước khi bước vào kì thi.
-        </div>
+          <div className="exam-infor-panel">
+            <div className="Mul-title">Đề luyện thi THPT Quốc gia 2020 môn Lý - Đề số 1</div>
+            <div className="description">
+              Gửi đến các bạn học sinh lớp 12 đề luyện thi THPT Quốc gia 2020 môn Lý - Đề số 1 có đáp án do Học Tốt tổng hợp nhằm giúp các em có thêm tư liệu để tham khảo củng cố kiến thức trước khi bước vào kì thi.
+            </div>
         <div className="item-infor-panel">
           <div className="item-label">
             <div className="icon">
@@ -48,7 +46,7 @@ class MultipleChoiceExam extends React.Component {
             <div className="icon">
               <i className="far fa-calendar-alt" />
             </div>
-            <div className="label">Số lần tạm dừngi</div>
+            <div className="label">Số lần tạm dừng</div>
           </div>
           <div className="gwt-HTML">
             0 / Không
@@ -76,7 +74,6 @@ class MultipleChoiceExam extends React.Component {
           <button className="btn btn-primary" type="submit">làm bài</button>
         </div>
       </div>
-        </div>
       </MainContent>
     );
   }
--- src/components/body/detail-exam/MultipleChoiceResult.js
+++ src/components/body/detail-exam/MultipleChoiceResult.js
@@ -18,7 +18,41 @@ class MultipleChoiceResult extends React.Component {
     return (
       <MainContent>
         <div className='MultipleChoiceResult'>
-        MultipleChoiceResult
+        <div>
+          <div className="exam-result-panel">
+              <div className="emoji-result">
+                <img src="https://www.imgworlds.com/wp-content/uploads/2015/12/18-CONTACTUS-HEADER.jpg" id="exam-images"></img>
+              </div>
+            <div className="row-infor-panel">
+              <div className="exam-label">Tổng điểm</div>
+              <div className="exam-result">0.0/10 Điểm</div>
+            </div>
+            <div className="row-infor-panel">
+              <div className="exam-label">Điểm cao nhất</div>
+              <div className="exam-result">0.0/10 Điểm</div>
+            </div>
+            <div className="row-infor-panel">
+              <div className="exam-label">Số câu đúng</div>
+              <div className="exam-result">0/40</div>
+            </div>
+            <div className="row-infor-panel">
+              <div className="exam-label">Thời gian làm bài</div>
+              <div className="exam-result">00m 00s</div>
+            </div>
+            <div className="row-infor-panel">
+              <div className="exam-label">Ngày Thi</div>
+              <div className="exam-result">10/09/2020</div>
+            </div>
+          </div>
+          <div className="row-infor-panel">
+            <div className="item-left">
+              <button className="btn btn-primary">xem lại kết quả</button>
+            </div>
+            <div className="item-right">
+              <button className="btn btn-primary">làm lại</button>
+            </div>
+        </div>
+      </div>
         </div>
       </MainContent>
     );
