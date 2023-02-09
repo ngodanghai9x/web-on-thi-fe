5054015e119bff89ab77e663e505cc1ffdbffe29
Hai Ngo <ngodanghai9x@gmail.com>
2023-02-09 09:47:52 +0700

    Updates

---
+++ precache-manifest.8724024108bdd9c3c3fd20b91df8ede7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "769d0a0fc218c699b37ace1332b3d6db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "666c41331cae82494949",
+    "url": "/static/css/2.f771fbf9.chunk.css"
+  },
+  {
+    "revision": "8df5dc09aabfa983e2ee",
+    "url": "/static/css/main.56df5e18.chunk.css"
+  },
+  {
+    "revision": "666c41331cae82494949",
+    "url": "/static/js/2.322abd38.chunk.js"
+  },
+  {
+    "revision": "3d85f791bc362d60a26c0031a409afbe",
+    "url": "/static/js/2.322abd38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8df5dc09aabfa983e2ee",
+    "url": "/static/js/main.56c6fbea.chunk.js"
+  },
+  {
+    "revision": "c7e3b8cd5b0e465d4c79",
+    "url": "/static/js/runtime-main.0fa8b10e.js"
+  }
+]);
